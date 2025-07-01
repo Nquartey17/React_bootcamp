738832a7b59ebf1d88aba30d4df934c82c428f0d
Nquartey17 <niikwarteiq@gmail.com>
2025-07-01 12:52:41 -0400

    Adding, deleting, and updating immutable arrays using spread, filter, and map

--- ultimate-react-course-main/02-JS-review/starter/script.js
+++ ultimate-react-course-main/02-JS-review/starter/script.js
@@ -293,3 +293,23 @@ sorted;
 
 const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
 sortedByPages;
+
+//immutable arrays
+// 1) Add a book to array (spread)
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J. K. Rowling",
+};
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) Delete book from array (filter)
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3) Update book in array (map)
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, pages: 1210 } : book
+);
+booksAfterUpdate;
